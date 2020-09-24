4c8dd035d650220accc8f86177d5b405295ee359
guyuezhai <guyue_fei@163.com>
2020-09-24 22:59:05 +0800

    :ambulance: 优化redux状态控制，增加跳转公众号页面

---
+++ src/pages/article/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import { WebView,View} from '@tarojs/components'
+
+
+
+function Article(){
+
+  return (
+    <View>
+      <WebView src='https://mp.weixin.qq.com/s/fUjlwNbXjv4sV8lrfTqRkA' ></WebView>
+    </View>
+
+  )
+}
+export default  Article
+
